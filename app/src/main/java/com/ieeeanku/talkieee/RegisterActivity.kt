b92de1bfadcbb5228f1dba889eb7abe451f4ab4e
emrenayir <emrenayir49@gmail.com>
2019-07-22 04:05:48 +0300

    Layouts
    Register layout added
    Login layout added
    Notifications activity added
    Notifications added

---
+++ app/src/main/java/com/ieeeanku/talkieee/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.ieeeanku.talkieee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
